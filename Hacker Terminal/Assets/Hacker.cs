ff2ac962548cea9942739cf847f80252aba1a2bc
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-09-21 17:14:14 -0300

    Terminal.WriteLine("Code");
    Using Terminal.WriteLine to apply on the game.

---
+++ Hacker Terminal/Assets/Hacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Game Initiated");
+        print("Welcome Master");
+        Terminal.WriteLine("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
