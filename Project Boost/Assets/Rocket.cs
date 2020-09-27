b14c50aef1172a57993915d64abf8bf206cec611
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-09-26 21:56:38 -0300

    Basic Input Binding

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust 
+        {
+            print("Space is pressed");
+        }
+
+        else if (Input.GetKey(KeyCode.A)) // can rotate left
+        {
+            print("A is pressed");
+        }
+        else if (Input.GetKey(KeyCode.D)) //can rotate right
+        {
+            print("d is pressed");
+        }
+    }
+}
